754121dcf5b1efee6e362f9eb4fddb784f39fcc8
TheSRK <shargo3554@gmail.com>
2019-10-02 00:35:58 +0600

    fixed skill row issue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".circularProgress").each(function(index, cProgress) {
     var progressPerntage = parseInt($(cProgress).attr("data-progress"));
+
     $(cProgress).css({
       "background-image":
         "conic-gradient(#dda908" +
@@ -8,8 +9,6 @@ $(document).ready(function() {
         "%,#0b090c " +
         progressPerntage +
         "%)"
-        
     });
-    
   });
 });
