c97f6104569aa078c823c4ad389ace89dd30a837
TheSRK <shargo3554@gmail.com>
2019-10-02 13:33:24 +0600

    javascript added to Skills section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   $(".circularProgress").each(function(index, cProgress) {
     var progressPerntage = parseInt($(cProgress).attr("data-progress"));
-
+  console.log(cProgress);
     $(cProgress).css({
-      "background-image":
-        "conic-gradient(#dda908" +
-        progressPerntage +
-        "%,#0b090c " +
-        progressPerntage +
-        "%)"
+      // "background-image":
+      //   "conic-gradient(#dda908" +
+      //   progressPerntage +
+      //   "%,#0b090c " +
+      //   progressPerntage +
+      //   "%)"
+      'background': "conic-gradient(#dda908 "+progressPerntage+"%,  #0b090c 0)"
+
     });
   });
 });
